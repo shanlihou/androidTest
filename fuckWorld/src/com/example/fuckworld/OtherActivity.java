21ba2f36564c2bdc131f835dc3f0178f0c90c650
shanlihou <shanlihou@gmail.com>
2014-07-23 07:37:09 +0800

    first commit

---
+++ fuckWorld/src/com/example/fuckworld/OtherActivity.java
@@ -0,0 +1,21 @@
+package com.example.fuckworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OtherActivity extends Activity {
+	
+	private MyApp myApp;
+	private TextView textView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.other);
+		textView = (TextView)this.findViewById(R.id.msg);
+		myApp = (MyApp)getApplication();
+		textView.setText("-appname-->>"+myApp.getName());
+	}
+
+}
